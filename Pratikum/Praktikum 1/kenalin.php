290228778e375f45f71fedecc5ad5b5f6f2acc8f
affry <61834849+affry@users.noreply.github.com>
2020-10-26 16:44:37 +0700

    Add files via upload

---
+++ Pratikum/Praktikum 1/kenalin.php
@@ -0,0 +1,10 @@
+<?php
+class kenalin extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Muhammad Affry<br>
+                saya tinggal di Brebes<br>
+                saya suka membaca & bermain komputer";
+    }
+
+}
---
+++ Pratikum/Praktikum 4/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad </li>
+        <li>Nama Belakang : Affry </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Gandasuli rt/rw 002/005 Kec.Brebes kab.Brebes</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Brebes</li></ul>
+
+    <h4>Kegiatan Favorit</h4>
+    <ul type="square">
+        <li>Membaca</li>
+        <li>Bermain komputer</li>
+    </ul>
+</section>
